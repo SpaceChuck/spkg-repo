acd73d0af4e5136f41f8a3c9bbd0c7763974444d
SpaceChuck <60058320+SpaceChuck@users.noreply.github.com>
2022-02-04 19:31:19 +0300

    Create install.py

---
+++ snakeos-update/install.py
@@ -0,0 +1,7 @@
+import requests
+
+
+url = "https://raw.githubusercontent.com/SpaceChuck/spkg-repo/snakeos-update/snake-update.py"
+r = requests.get(url, allow_redirects=True)
+
+open('exec/snakeos-update.py', 'wb').write(r.content)
