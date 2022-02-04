7e5cd49faddb4eec1e45b473297e9b70a1552c7c
SpaceChuck <60058320+SpaceChuck@users.noreply.github.com>
2022-02-04 17:09:11 +0300

    Update install.py

--- examples/install.py
+++ examples/install.py
@@ -1,1 +1,7 @@
+import requests
 
+
+url = "https://raw.githubusercontent.com/SpaceChuck/SnakeOS/main/exec/examples.py"
+r = requests.get(url, allow_redirects=True)
+
+open('exec/examples.py', 'wb').write(r.content)
